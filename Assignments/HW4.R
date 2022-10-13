18462580a3f068d2ada6028a256806ca1c1d8a34
Maria K <maria.kunevich@gmail.com>
2022-10-13 18:34:17 +0300

    Create HW4.R

---
+++ Assignments/HW4.R
@@ -0,0 +1,23 @@
+##############################
+##  Homework assignment #4  ##
+##############################
+
+# Your full name: 
+# Date:
+
+####### TASK 1: 5 points. Short EDA report ###############
+
+# The main aim of the task is to import data into R, perform brief explanatory analysis, build at least one plot,
+# export data using R Markdown and 'knit' by creating an html.file
+
+# 1. Go to https://andmed.stat.ee/en/stat and create a data set you want to explore. Import the data into R.
+# 2. Prepare EDA (Explanatory Data Analysis). Write at least 5 questions and provide your answers during the first stage of EDA.
+# 3. Provide brief descriptive statistical analysis of your data set (like measures of central tendency and dispersion).
+# 4. Include at least one plot into your report. 
+# If ggplot2 is too complicated for you now, create a plot with R base functions.
+# 5. Create a pdf or html file with short EDA of your data set. Send a pdf file directly to me, send html code
+# through a gist on GitHub.
+
+
+################# END OF HOME ASSIGNMENT ################
+
