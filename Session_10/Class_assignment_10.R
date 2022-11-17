0aa6c7dbf87c5846e5187bf8c234ce5d5683f869
Maria K <maria.kunevich@gmail.com>
2022-11-17 14:17:07 +0200

    Create Class_assignment_10.R

---
+++ Session_10/Class_assignment_10.R
@@ -0,0 +1,23 @@
+##### Class assignment
+##### Session 10
+
+# Create a data frame with three columns - name, salary, expenses
+# and 10 rows for people: 
+
+### Step 1. Briefly describe your data set.
+### Step 2. Perform a quick EDA  
+### Step 3. Hypothesis testing 
+
+
+#### Step 1.  
+# draw a scatterplot
+# State a null hypothesis and alternate hypothesis
+
+#### Step 2. Collect data
+
+### Step 3. Select an appropriate statistical test; check the assumptions
+
+### Step 4. Calculate the p-value, select significance level (1%, 5%). 
+# Decide whether to reject or fail to reject your null hypothesis. 
+
+### Step 5. Report your results
